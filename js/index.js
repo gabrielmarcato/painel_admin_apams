d4356a8a3b787172f635144b9ca61f9a55f3edc2
gabrielmarcato <gabrielomarcato@gmail.com>
2019-05-30 14:42:10 -0400

    Ajuste configPage

--- js/index.js
+++ js/index.js
@@ -1,8 +1,26 @@
+$('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').trigger('focus')
+})
+
+window.onload = function () {
+  //Busca usuario
+  var filtro = document.getElementById('procurarAnimais');
+  var tabela = document.getElementById('tabelaAnimais');
+  filtro.onkeyup = function () {
+    var nomeFiltro = filtro.value;
+    for (var i = 1; i < tabela.rows.length; i++) {
+      var conteudoCelula = tabela.rows[i].cells[2].innerText;
+      var corresponde = conteudoCelula.toLowerCase().indexOf(nomeFiltro) >= 0;
+      tabela.rows[i].style.display = corresponde ? '' : 'none';
+    }
+  };
+}
+
 window.URL = window.URL || window.webkitURL;
 
 const fileSelect = document.getElementById("fileSelect"),
-    fileElem = document.getElementById("fileElem"),
-    fileList = document.getElementById("fileList");
+  fileElem = document.getElementById("fileElem"),
+  fileList = document.getElementById("fileList");
 
 fileSelect.addEventListener("click", function (e) {
   if (fileElem) {
@@ -21,18 +39,48 @@ function handleFiles(files) {
     for (let i = 0; i < files.length; i++) {
       const li = document.createElement("li");
       list.appendChild(li);
-      
+
       const img = document.createElement("img");
       img.src = window.URL.createObjectURL(files[i]);
       img.height = 60;
-      img.onload = function() {
+      img.onload = function () {
         window.URL.revokeObjectURL(this.src);
       }
       li.appendChild(img);
       const info = document.createElement("span");
       info.innerHTML = files[i].name;
       li.appendChild(info);
-      
+
     }
   }
 }
+
+var  table = document.getElementById("tabelaAnimais")
+
+
+// display selected row data into input text
+function selectedRowToInput() {
+
+  for (var i = 1; i < table.rows.length; i++) {
+    table.rows[i].onclick = function () {
+      // get the seected row index
+      document.getElementById("nomeUsuario").value = this.cells[1].innerHTML;
+      document.getElementById("emailUsuario").value = this.cells[2].innerHTML;
+      document.getElementById("nivelUsuario").value = this.cells[3].innerHTML;
+    };
+  }
+}
+selectedRowToInput();
+function editHtmlTbleSelectedRow() {
+  var codigoUsuario = document.getElementById("codigoUsuario").value,
+    nomeUsuario = document.getElementById("nomeUsuario").value,
+    emailUsuario = document.getElementById("emailUsuario").value,
+    nivelUsuario = document.getElementById("nivelUsuario").value;
+  if (!checkEmptyInput()) {
+    table.rows[rIndex].cells[0].innerHTML = codigoUsuario;
+    table.rows[rIndex].cells[1].innerHTML = nomeUsuario;
+    table.rows[rIndex].cells[2].innerHTML = emailUsuario;
+    table.rows[rIndex].cells[3].innerHTML = nivelUsuario;
+  }
+}
+
