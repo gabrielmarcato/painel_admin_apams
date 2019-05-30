367f507a026ea1899d4dc71a9a33ab523d1ca0fe
gabrielmarcato <gabrielomarcato@gmail.com>
2019-05-30 11:25:17 -0400

    Ajuste configPage

--- js/index.js
+++ js/index.js
@@ -1,3 +1,38 @@
-document.querySelector('.button').addEventListener('click', () => {
-  document.querySelector('.content').classList.toggle('isOpen');
-});
+window.URL = window.URL || window.webkitURL;
+
+const fileSelect = document.getElementById("fileSelect"),
+    fileElem = document.getElementById("fileElem"),
+    fileList = document.getElementById("fileList");
+
+fileSelect.addEventListener("click", function (e) {
+  if (fileElem) {
+    fileElem.click();
+  }
+  e.preventDefault(); // prevent navigation to "#"
+}, false);
+
+function handleFiles(files) {
+  if (!files.length) {
+    fileList.innerHTML = "<p>No files selected!</p>";
+  } else {
+    fileList.innerHTML = "";
+    const list = document.createElement("ul");
+    fileList.appendChild(list);
+    for (let i = 0; i < files.length; i++) {
+      const li = document.createElement("li");
+      list.appendChild(li);
+      
+      const img = document.createElement("img");
+      img.src = window.URL.createObjectURL(files[i]);
+      img.height = 60;
+      img.onload = function() {
+        window.URL.revokeObjectURL(this.src);
+      }
+      li.appendChild(img);
+      const info = document.createElement("span");
+      info.innerHTML = files[i].name;
+      li.appendChild(info);
+      
+    }
+  }
+}
